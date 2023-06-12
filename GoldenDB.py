71fde57bf25f2b9233587beb1be06b7cd0db899e
GodsmercyStudios <100350473+GodsmercyStudios@users.noreply.github.com>
2023-06-12 16:44:17 +0100

    Added "search" function

--- GoldenDB.py
+++ GoldenDB.py
@@ -19,14 +19,22 @@ class GoldenDB():
         del database[key]
     
     def save(filename):
+        # save contents of database in JSON
         with open(filename, "w") as fh:
             json.dump(database, fp)
 
     def load(filename):
+        # load contents of JSON into database
         with open(filename, "r") as fh:
             database = json.load(fp)
     
     def listall():
         return list(database)
     
+    def search(query):
+        if query in database:
+            return database[query]
+        else:
+            return "Not in database"
     
+        

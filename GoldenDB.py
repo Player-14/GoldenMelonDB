f801f1f5bc62dca47c903dd92ba25a5a0a9e13a0
Player-14 <100350473+Player-14@users.noreply.github.com>
2023-07-05 17:18:17 +0100

    v1.1.1
    Added better documentation for code.

--- GoldenDB.py
+++ GoldenDB.py
@@ -1,4 +1,4 @@
-# Simple NoSQL Python Database
+# GoldenMelonDB NoSQL Python Database
 
 class GoldenDB():
     def __init__(self, database):
@@ -6,6 +6,7 @@ class GoldenDB():
         self.database = {}
 
     def newtable(self, table):
+        # Create nested dictionary as table
         self.database.update({table:{}})
 
     def insert(self, table, key, value):
@@ -13,6 +14,7 @@ class GoldenDB():
         self.database[table].update({key:value})
     
     def get(self, table, key):
+        # gets the value for a particular key
         container = self.database[table].get(key, None)
         return container
     
@@ -33,7 +35,7 @@ class GoldenDB():
             with open(filename, "r") as fh:
                 self.database = ujson.load(fh)
         except FileNotFoundError:
-            return "File {filename} Not Found!"
+            return f"File {filename} Not Found!"
     
     def listall(self, table):
         # list all contents of database
@@ -47,20 +49,25 @@ class GoldenDB():
             self.database[table] = {}
     
     def search(self, table, query):
+        # searches a table in the database for a specific key/value
         if query in self.database:
             return self.database[table][query]
         else:
             return "Not in database"
+
     def uninsert(self):
-        # remove last inserted object
+        # removes the last inserted object
         self.database[table].popitem()
 
     def countdb(self, table):
-        return len(self.database[table].items())
+        # count the amount of items in the database
+        return len(self.database[table].keys())
 
     def ininsert(self, table, key, value):
+        # inserts data if data doesn't exist
         return self.database[table].setdefault(key, value)
 
     def tables(self):
+        # returns a list of all of the tables
         return self.database.keys()
     
